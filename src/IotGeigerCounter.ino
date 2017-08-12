5be8a988a44738be1115c8b38738ff8e736cf587
Brian Leschke <Bleschke@users.noreply.github.com>
2017-08-12 09:40:56 -0400

    Update IotGeigerCounter.ino

--- src/IotGeigerCounter.ino
+++ src/IotGeigerCounter.ino
@@ -1,6 +1,6 @@
 /*  
 * Brian Leschke 
-* August 11, 2017 
+* August 12, 2017 
 * Adafruit Feather Huzzah IoT Geiger Counter 
 * An Adafruit Feather Huzzah with a Feather OLED expansion will be connected to a MightyOhm Geiger Counter (kit). 
 * The Feather Huzzah is programmed to display uS/hr, CPS, and CPM on both a webpage and the oled screen. 
@@ -14,6 +14,6 @@
 *  
 * 8/9/2017  - Formed idea, sketched project on paper. Supplies purchased 
 * 8/11/2017 - Geiger Counter built 
-*  
+* 8/12/2017 - Started merging code 
 *  
 **/
